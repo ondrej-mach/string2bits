8f4d73e08e34af6c8ff823f73105c08157ebdbb9
Ondřej Mach <ondrej.mach@seznam.cz>
2021-01-28 12:25:57 +0100

    Should be working
    Now using python integrated function to convert to binary.

--- decoder.py
+++ decoder.py
@@ -2,24 +2,24 @@
 import sys
 
 encodingMap = {
-    0: ':SeriousSloth:',
-    1: ':panik:'
+    '0': ':SeriousSloth:',
+    '1': ':panik:'
 }
 
 inputString = sys.stdin.read()
 decodedString = ''
 
-# empty list to store the decoded bits
-bits = []
+# empty string of decoded bits
+bits = ''
 
 while True:
     # try to find both symbols in the string
-    # the closer one is the bit we are trying to read
+    # the closer one represents the next bit
     nearest = None
     minDistance = len(inputString)
 
     for key in encodingMap.keys():
-        # search for the value
+        # search for the substring
         distance = inputString.find(encodingMap[key])
         if (distance != -1) and (distance < minDistance):
             minDistance = distance
@@ -28,17 +28,15 @@ while True:
     if nearest is None:
         break
 
-    bits.append(nearest)
+    bits += nearest
     newStart = minDistance + len(encodingMap[nearest])
     inputString = inputString[newStart:]
 
-while len(bits) > 8:
-    # integer representation of ascii
-    asciiNumber = 0
-    for i in range(8):
-        # set apprpriate bits in final number
-        asciiNumber = asciiNumber | (bits.pop(0) << i)
-
-    decodedString += chr(asciiNumber)
+while len(bits) >= 8:
+    # read the first 8 characters and convert them into integer
+    byte = int(bits[:8], 2)
+    # delete the first 8 characters from the string
+    bits = bits[8:]
+    decodedString += chr(byte)
 
 print(decodedString)
--- encoder.py
+++ encoder.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import sys
 
 # what should be written in output instead of original bytes
 encodingMap = {
-    0: ':SeriousSloth:',
-    1: ':panik:'
+    '0': ':SeriousSloth:',
+    '1': ':panik:'
 }
 # inbetween the bits
 bitSeparator = ' '
@@ -12,20 +13,22 @@ byteSeparator = '\n'
 # both separators are only cosmetic, decoder ignores them
 # make sure, that separators do not contain any string from encodingMap
 
-inputString = input()
+inputString = sys.stdin.read()
 encodedString = ''
 
 for character in inputString:
     # convert every character of the string into an integer
-    num = ord(character)
-    # convert the integer into list of corresponding bits
-    for i in range(8)
-        # shift the bit in place of 1's
-        bit = (num >> i) & 1
-        # result is already 0 or 1
-        # convert into fila string
-        encodedString += outputMap[bit] + bitSeparator
+    byte = ord(character)
+    # convert the byte into string of 1's and 0's
+    binaryString = format(byte, '#010b')
+    # remove '0b' prefix
+    binaryString  = binaryString[2:]
 
+    for bit in binaryString:
+        # write all bits in the final encoded string
+        encodedString += encodingMap[bit]
+        encodedString += bitSeparator
+    # byteSeparator after each character
     encodedString += byteSeparator
 
 print(encodedString)
